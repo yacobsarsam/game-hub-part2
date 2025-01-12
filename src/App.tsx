6a4d4ffc91f73b68588206b02c4924f3b80b809c
Yacoub Sarsam <yacoub.sarsam@digitalengine.se>
2025-01-12 15:46:14 +0100

    Build a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {Grid, GridItem, useBreakpointValue,} from "@chakra-ui/react";
+import NavBar from "@/components/NavBar.tsx";
 
 function App() {
     const isLargeScreen = useBreakpointValue({ base: false, lg: true });
@@ -7,7 +8,9 @@ function App() {
         base: `"nav" " main"`,
         lg:`"nav nav" "aside main"`
     }}>
-        <GridItem area="nav" bg="coral">Nav</GridItem>
+        <GridItem area="nav" bg="coral">
+            <NavBar/>
+        </GridItem>
         {isLargeScreen && <GridItem area="aside" bg="gold">Aside</GridItem>}
         <GridItem area="main" bg="dodgerblue">Main</GridItem>
     </Grid>
