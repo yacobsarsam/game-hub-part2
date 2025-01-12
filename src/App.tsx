838287d0153a16a75b0ffbc6d1b62faf645d579c
Yacoub Sarsam <yacoub.sarsam@digitalengine.se>
2025-01-12 15:24:36 +0100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-
-import { Button} from "@chakra-ui/react"
-
+import {Grid, GridItem, useBreakpointValue,} from "@chakra-ui/react";
 
 function App() {
-    return <Button colorPalette ={'blue'}> Click me</Button>
+    const isLargeScreen = useBreakpointValue({ base: false, lg: true });
+
+    return <Grid templateAreas={{
+        base: `"nav" " main"`,
+        lg:`"nav nav" "aside main"`
+    }}>
+        <GridItem area="nav" bg="coral">Nav</GridItem>
+        {isLargeScreen && <GridItem area="aside" bg="gold">Aside</GridItem>}
+        <GridItem area="main" bg="dodgerblue">Main</GridItem>
+    </Grid>
 }
 
 export default App
