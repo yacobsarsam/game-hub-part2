8789144ccb8f9936f7de120ce57257ab85619585
Yacoub Sarsam <yacoub.sarsam@digitalengine.se>
2025-01-15 21:52:04 +0100

    Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import NavBar from "@/components/NavBar.tsx";
 function App() {
     const isLargeScreen = useBreakpointValue({ base: false, lg: true });
 
-    return <Grid templateAreas={{
-        base: `"nav" " main"`,
+    return(
+    <Grid templateAreas={{
+        base: `"nav" "main"`,
         lg:`"nav nav" "aside main"`
-    }}>
-        <GridItem area="nav" bg="coral">
+        }}>
+        <GridItem area="nav">
             <NavBar/>
         </GridItem>
         {isLargeScreen && <GridItem area="aside" bg="gold">Aside</GridItem>}
         <GridItem area="main" bg="dodgerblue">Main</GridItem>
     </Grid>
+    );
 }
-
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from "@/components/ui/provider"
-
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import theme from './theme'; // Ensure the theme file path is correct
+import App from './App';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <Provider>
-    <App />
-          </Provider>
-  </StrictMode>,
-)
+    <StrictMode>
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <App />
+        </ChakraProvider>
+    </StrictMode>,
+);
