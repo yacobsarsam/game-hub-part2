1118bcf9d211f9296c24b3859fb05afff52fe2c4
Yacoub Sarsam <yacoub.sarsam@digitalengine.se>
2025-01-18 17:09:33 +0100

    Fetch the genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {Grid, GridItem, useBreakpointValue,} from "@chakra-ui/react";
 import NavBar from "@/components/NavBar.tsx";
 import GameGrid from "@/components/GameGrid.tsx";
+import GenreList from "@/components/GenreList.tsx";
 
 function App() {
     const isLargeScreen = useBreakpointValue({base: false, lg: true});
@@ -13,7 +14,9 @@ function App() {
             <GridItem area="nav">
                 <NavBar/>
             </GridItem>
-            {isLargeScreen && <GridItem area="aside">Aside</GridItem>}
+            {isLargeScreen && <GridItem area="aside">
+            <GenreList/>
+            </GridItem>}
             <GridItem area="main">
                 <GameGrid/>
             </GridItem>
