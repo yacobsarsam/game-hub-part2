13bcd5d076eacd620c9c8a4d6c7f8aee824b890e
Yacoub Sarsam <yacoub.sarsam@digitalengine.se>
2025-01-23 19:13:18 +0100

    Refactor GameGrid.tsx

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,21 +12,21 @@ const GameGrid = ({gameQuery}:Props) => {
 
     const skeletons = [1,2,3,4,5,6,7,8,9,10];
         const {data, error,isLoading}= useGames(gameQuery);
-        return (
-            <>
-                { error && <Text>{error}</Text> }
-                <SimpleGrid columns={{sm:1, md:2 , lg:3 , xl:4}} spacing={6} padding={5}  >
-                    {isLoading && skeletons.map(skeleton=>(
-                        <GameCardContainer key={skeleton}>
-                            <GameCardSkeleton />
-                        </GameCardContainer>))}
-                    {data.map((game)=>(
-                        <GameCardContainer key={game.id}>
-                            <Game_Card  game={game}></Game_Card>
-                        </GameCardContainer>
-                    ))}
-                </SimpleGrid>
-            </>
+
+        if (error) return <Text>Failed to load games</Text>;
+
+    return (
+            <SimpleGrid columns={{sm:1, md:2 , lg:3 , xl:4}} spacing={6} padding={5}  >
+                {isLoading && skeletons.map(skeleton=>(
+                    <GameCardContainer key={skeleton}>
+                        <GameCardSkeleton />
+                    </GameCardContainer>))}
+                {data.map((game)=>(
+                    <GameCardContainer key={game.id}>
+                        <Game_Card  game={game}></Game_Card>
+                    </GameCardContainer>
+                ))}
+            </SimpleGrid>
         )
 }
 export default GameGrid;
