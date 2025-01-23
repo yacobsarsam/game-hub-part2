a7b703b0f21a23f959b6acbe2b5c19a456950f41
Yacoub Sarsam <yacoub.sarsam@digitalengine.se>
2025-01-23 18:46:14 +0100

    Ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,12 @@
-
-import useData from "@/hooks/useData.ts";
+import genres from "../data/genres.ts";
+//import useData from "@/hooks/useData.ts";
 
 export interface Genre {
     id:number;
     name:string;
     image_background :string;
 }
-const useGenres = () => useData<Genre>('/genres');
+//const useGenres = () => useData<Genre>('/genres');
+const useGenres = () => ({data:genres,error:null,isLoading:false});
+
 export default useGenres;
